a00c3fa97431231069fb9a283f1a66320cb80896
INeedJobToStartWork <ineedjobtostartwork@gmail.com>
2024-06-29 20:15:31 +0200

    ⚙️  Chore(config/eslintrc): add config for `base.js`, `stylistic-ts.js`,`stylistic.js`,`typescript.js` + add `start` to tasks at `turbo.json` + add Tsconfig basic files

---
+++ config/eslintrc/eslint.config.js
@@ -0,0 +1,6 @@
+import Base from "./base.js"
+import Typescript from "./typescript.js"
+import Stylistic from "./stylistic.js"
+import StylisticTS from "./stylistic-ts.js"
+
+export default [...Base,...Typescript,...Stylistic, ...StylisticTS]
