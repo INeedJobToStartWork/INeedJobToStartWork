828e505177a6bf3c97b8697f0485c05df0231b3f
INeedJobToStartWork <ineedjobtostartwork@gmail.com>
2024-07-05 00:23:21 +0200

    ⚙️  Chore(@ineedj/tsuprc,myplop): add `@ineedj/tsuprc` and some changes in `myplop`

---
+++ config/tsuprc/tsup.base.ts
@@ -0,0 +1,10 @@
+import { copy } from 'esbuild-plugin-copy';
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  target: 'es2020',
+  format: ['esm'],
+  clean: true,
+  splitting: false,
+});
