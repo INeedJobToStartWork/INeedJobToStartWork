632a9d4c6664beae819686b0f45368288dc6c8c4
INeedJobToStartWork <ineedjobtostartwork@gmail.com>
2024-07-06 18:34:58 +0200

    🎨 Style(index.ts): change path import to correct

--- config/eslintrc/src/rules/formatters/index.ts
+++ config/eslintrc/src/rules/formatters/index.ts
@@ -1,5 +1,5 @@
-export { default as JSON } from "./json";
-export { default as perfectionistSorters } from "./perfectionistSorters";
-export { default as stylistic } from "./stylistic";
-export { default as stylisticJSX } from "./stylisticJSX";
-export { default as stylisticTS } from "./stylisticTS";
+export { default as JSON } from './json';
+export { default as perfectionistSorters } from './perfectionistSorters';
+export { default as stylistic } from './stylistic';
+export { default as stylisticJSX } from './stylisticJsx';
+export { default as stylisticTS } from './stylisticTs';
